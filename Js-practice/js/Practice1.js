e85be691572726499fccb1b12874d9d118c09986
Jayson-PAmano <jaysonpa36@gmail.com>
2025-07-11 00:22:41 -0700

    Add functions and loops for array operations
    Introduced several arrow functions for greeting, summing, doubling, and checking even/odd. Added loops to iterate over fruits and colors arrays, including forward and reverse order, and implemented while loops with counters.

--- Js-practice/js/Practice1.js
+++ Js-practice/js/Practice1.js
@@ -1,6 +1,9 @@
 let fruits = ["apple", "orange", "grapes"];
 let colors = ["red", "green", "blue", "yellow"];
 
+let counter = 0;
+let counter2 = 5;
+
 let numbers = [1, 2, 3, 4];
 let squaredNum = [];
 const person = [
@@ -22,7 +25,59 @@ const person = [
 ];
 
 //functions
+getName = (myName) => console.log(`Hello, ${myName}`);
+getName("John");
+
+getSum = (x, y) => x + y;
+console.log(getSum(5, 7));
+
+double = (x, y) => {
+  return getSum(x, y) * 2;
+};
+console.log(double(5, 7));
+
+let results;
+
+Remainder = (remainder) => {
+  if (remainder % 2 == 0) {
+    results = "even";
+    return results;
+  } else {
+    results = "odd";
+    return results;
+  }
+};
+console.log(Remainder(3));
+
+//loops
 for (i = 0; i < numbers.length; i++) {
   squaredNum[i] = numbers[i] * numbers[i];
 }
 console.log(squaredNum);
+
+for (i = 0; i < fruits.length; i++) {
+  logFruits(fruits[i], i);
+}
+
+function logFruits(element, index) {
+  console.log(`Fruit at index ${index}: ${element}`);
+}
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+console.log("\n");
+while (counter2 >= 0) {
+  console.log(counter2);
+  counter2--;
+}
+
+for (i = 0; i < colors.length; i++) {
+  console.log(i + colors[i]);
+}
+
+console.log("Color Reverse:");
+
+for (i = colors.length - 1; i >= 0; i--) {
+  console.log(i + colors[i]);
+}
