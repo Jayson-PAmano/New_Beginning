fc700ec7afde58fc55e4c9b373d340b9df4bdcde
Jayson-PAmano <jaysonpa36@gmail.com>
2025-07-16 22:03:10 -0700

    Add Practice4.js and update script reference
    Created Practice4.js demonstrating JS string methods (concat and slice). Updated index.html to reference Practice4.js instead of Practice3.js.

---
+++ Js-practice/js/Practice4.js
@@ -0,0 +1,11 @@
+//JS String Methods
+
+let text1 = "Hello";
+let text2 = "World";
+
+let text3 = text1.concat(text2);
+console.log(text3);
+
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(1, 7);
+console.log(part);
