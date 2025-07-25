529f2c9d13163f7694c2abb18c33d5295bbcb156
Jayson-PAmano <jaysonpa36@gmail.com>
2025-07-24 22:56:34 -0700

    Add various loop and array method practice examples
    Introduced multiple loop constructs (while, for, for...in, for...of) and array methods (forEach, map, filter) for practice. Added new arrays and demonstrated looping through arrays and objects, as well as basic operations like squaring numbers and filtering values. Commented out unused prompt-based code for clarity.

--- Js-practice/js/Practice7.js
+++ Js-practice/js/Practice7.js
@@ -6,7 +6,9 @@ let counter = 0;
 let counter2 = 5;
 
 let numbers = [1, 2, 3, 4];
+let numbers2 = [21, 32, 13, 54];
 let squaredNum = [];
+let Results = [];
 
 const letters = ["a", "b", "c", "d"];
 const letters2 = ["e", "f", "g", "h"];
@@ -40,22 +42,72 @@ getRemainder = (r1) => {
 };
 
 console.log(getRemainder(7));
-let answer;
-getRemainder2 = () => {
-  let r1 = prompt("Enter your number: ");
-  const number = parseInt(r1);
-  if (isNaN(number)) {
-    return "Invalid input. Please enter a number.";
-  }
-  if (r1 % 2 == 0) {
-    answer = "EVEN";
-  } else {
-    answer = " ODD";
-  }
-  return answer;
-};
+// let answer;
+// getRemainder2 = () => {
+//   let r1 = prompt("Enter your number: ");
+//   const number = parseInt(r1);
+//   if (isNaN(number)) {
+//     return "Invalid input. Please enter a number.";
+//   }
+//   if (r1 % 2 == 0) {
+//     answer = "EVEN";
+//   } else {
+//     answer = " ODD";
+//   }
+//   return answer;
+// };
 
-console.log(getRemainder2(answer));
+// console.log(getRemainder2(answer));
+
+//Loops
+
+while (counter <= 5) {
+  while (counter2 >= 0) {
+    console.log(`\t ${counter2}`);
+    counter2--;
+  }
+  console.log(counter);
+  counter++;
+}
+
+for (let i = 0; i < colors.length; i++) {
+  console.log(`Postion: ${i}\n Color: ${colors[i]}`);
+}
+
+console.log("\n Reverse: ");
+for (let i = colors.length - 1; i >= 0; i--) {
+  console.log(`Postion: ${i}\n Color: ${colors[i]}`);
+}
+
+for (i = 0; i < text.length; i++) {
+  console.log(text.charAt(i).toUpperCase());
+  if (text.charAt(i) === "e") {
+    console.log(`Found E at Position ${i}`);
+    console.log("break");
+    break;
+  }
+}
+
+for (i = 0; i < fruits.length; i++) {
+  getFruits(fruits[i], i);
+}
+function getFruits(element, index) {
+  console.log(`Fruit at position ${index}: ${element}`);
+}
+
+for (i = 0; i < numbers.length; i++) {
+  // squaredNum[i] = numbers[i] * numbers[i];
+  squaredNum[i] = Math.round(Math.sqrt(numbers[i]));
+}
+console.log(squaredNum);
+
+for (i = 0; i < numbers2.length; i++) {
+  if (numbers2[i] < 21) {
+    console.log(numbers2[i]);
+    // Results.push(numbers2[i]);
+  }
+}
+// console.log(Results);
 
 // Objects
 
@@ -103,3 +155,38 @@ const Teams = {
     console.log(`Teams:\n ${teams.T1} \n ${teams.T2} \n ${teams.T3}`);
   },
 };
+
+//Looping Objects
+
+for (i = 0; i < person.length; i++) {
+  console.log(person[i].firstName);
+}
+city.callCities();
+Teams.callTeams.call(teams);
+
+for (car in cars) {
+  console.log(cars[car]);
+}
+
+for (p of person) {
+  console.log(`Name: ${p.firstName}`);
+}
+for (f of colors) {
+  console.log(`Color: ${f}`);
+}
+
+//ForEach practice
+
+fruits.forEach((element, index) => {
+  console.log(`Fruit: ${element} at index ${index}`);
+});
+
+//Map practice
+
+const squaredNum2 = numbers2.map((value) => {
+  console.log((value * value) / 2);
+});
+
+const filtedNum = numbers2.filter((e) => {
+  console.log(e > 10);
+});
