fa79a5881792b67cbc33385da35d62efe4dcccfa
Jayson-PAmano <jaysonpa36@gmail.com>
2025-07-31 23:14:15 -0700

    Add loop examples and commented number check code
    Added various loop constructs (while, for) to demonstrate iteration over counters, arrays, and strings. Also included commented-out code for checking if a user input number is even or odd.

--- Js-practice/js/Practice9.js
+++ Js-practice/js/Practice9.js
@@ -30,6 +30,17 @@ callNames = () => {
 callNames();
 console.log("\n");
 
+// let r1 = prompt("Enter your Number");
+// let number = parseInt(r1);
+// if (isNaN(number)) {
+//   console.log("Invalid Number");
+// }
+// if (r1 % 2 == 0) {
+//   console.log("Your Number is even");
+// } else {
+//   console.log("Your Number is odd");
+// }
+
 // let Text = prompt("Enter Text");
 // let index = Text.indexOf();
 // console.log(`You entered: "${Text}" the total index of that word is ${index}`);
@@ -65,3 +76,40 @@ doubleSum = (a, b) => {
   console.log(sum);
 };
 doubleSum(5, 10);
+console.log("\n");
+// Loops
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+console.log("\n");
+
+while (counter2 >= 0) {
+  console.log(counter2);
+  counter2--;
+}
+
+console.log("\n");
+for (let i = 0; i < fruits.length; i++) {
+  console.log(`index #: ${i} \n fruit: ${fruits[i]}`);
+}
+console.log("\n");
+
+for (let i = fruits.length - 1; i >= 0; i--) {
+  console.log(`index #: ${i} \n fruit: ${fruits[i]}`);
+}
+console.log("\n");
+for (let i = 0; i < colors.length; i++) {
+  console.log(`index #: ${i} \n color: ${colors[i]}`);
+
+  if (colors[i] === "green") {
+    console.log(`Found the COLOR: ${colors[i]} at POSITION: ${i}`);
+    console.log("STOP 🏳🏳‍🌈 ");
+    break;
+  }
+}
+console.log("\n");
+for (let i = 0; i < text.length; i++) {
+  console.log(" ---> " + text.charAt(i).toUpperCase());
+}
