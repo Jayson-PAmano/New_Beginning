a93d9420ae34a007c8adab897319699e5e50c9fe
Jayson-PAmano <jaysonpa36@gmail.com>
2025-08-06 23:55:44 -0700

    js practice objects

---
+++ Js-practice/js/Practice12.js
@@ -0,0 +1,63 @@
+let fruits = ["apple", "orange", "grapes"];
+let colors = ["red", "green", "blue", "yellow"];
+let text = "Heroes";
+
+let counter = 0;
+let counter2 = 5;
+
+let numbers = [1, 2, 3, 4];
+let squaredNum = [];
+
+const letters = ["a", "b", "c", "d"];
+const letters2 = ["e", "f", "g", "h"];
+
+const Cars = {
+  Toyota: {
+    model: "Camry",
+    year: 2015,
+    made_in: "China",
+  },
+  Honda: {
+    model: "Civic",
+    year: 2018,
+    made_in: "Japan",
+  },
+};
+
+const Animals = [
+  {
+    name: "Lion",
+    age: 5,
+  },
+  {
+    name: "Tiger",
+    age: 3,
+  },
+];
+
+const Country = {
+  Asia: [
+    {
+      name: "China",
+      population: 1439323776,
+    },
+    {
+      name: "India",
+      population: 1380004385,
+    },
+  ],
+  Europe: [
+    {
+      name: "Russia",
+      population: 5439323776,
+    },
+    {
+      name: "Moscow",
+      population: 2339323776,
+    },
+  ],
+};
+console.log(Country.Asia);
+console.log(Cars.Toyota.model);
+
+console.log(Animals[1]);
