43c14eb6ac280c36a683d7cb9572ebab619a104e
Jayson-PAmano <jaysonpa36@gmail.com>
2025-08-07 16:39:48 -0700

    simple js validation

--- Js-practice/js/Practice12.js
+++ Js-practice/js/Practice12.js
@@ -57,7 +57,117 @@ const Country = {
     },
   ],
 };
-console.log(Country.Asia);
-console.log(Cars.Toyota.model);
+//Start here....
 
-console.log(Animals[1]);
+let response;
+
+//1 Functions
+// getName = (name) => {
+
+//   while (name !== "Jaden") {
+//     name = prompt("Enter your name: ");
+//     if (name === "") {
+//       alert("Please, do not leave empty");
+//       response = "Please, do not leave empty";
+//     } else if (name === "Jaden") {
+//       alert("Welcome, Jaden");
+//       response = "Welcome, Jaden";
+//     } else if (name.startsWith("jaden")) {
+//       alert("Please use upper case of the first letter");
+//       response = "Please use upper case of the first letter";
+//     } else {
+//       alert(`Name ${name} is undefined`);
+//       response = `Name ${name} is undefined`;
+//     }
+//   }
+//   console.log(response);
+// };
+// getName();
+
+// getNum = (myNumber) => {
+//   myNumber = prompt("Enter your number");
+//   let number = parseInt(myNumber);
+//   if (isNaN(number)) {
+//     alert("Please enter a valid number");
+//     response = "Please enter a valid number";
+//   }
+
+// else if (number < 0) {
+//   alert("Please enter a positive number");
+//   response = "Please enter a positive number";
+// } else {
+//   alert(`Your number is ${number}`);
+//   response = `Your number is ${number}`;
+// }};getNum();
+
+//   while (myNumber < 10) {
+//     alert("Please enter a number greater than 10");
+//     response = "Please enter a number greater than 10";
+//     myNumber = prompt("Enter your number");
+//     number = parseInt(myNumber);
+//   }
+//
+// };
+
+let output = document.querySelector("h2");
+// AddNum = () => {
+//   let num1 = prompt("Enter first number: ");
+//   let num2 = prompt("Enter second number: ");
+//   let number1 = parseInt(num1);
+//   let number2 = parseInt(num2);
+//   if (isNaN(number1) || isNaN(number2)) {
+//     alert("Please enter a valid number");
+//     response = "Please enter a valid number";
+//     output.innerHTML = response;
+//   } else if (number1 < 0 || number2 < 0) {
+//     alert("Please enter a positive number");
+//     response = "Please enter a positive number";
+//     output.innerHTML = response;
+//   } else if (number1 > 100 || number2 > 100) {
+//     alert("Please enter a number less than 100");
+//     response = "Please enter a number less than 100";
+//     output.innerHTML = response;
+//   } else {
+//     let sum = number1 + number2;
+//     alert(`The sum of ${number1} and ${number2} is ${sum}`);
+//     response = `The sum of ${number1} and ${number2} is ${sum}
+//     `;
+
+//     alert(`The Square root  of ${sum} is ${Math.sqrt(sum)}`);
+//     response = `The Square root  of ${sum} is ${Math.sqrt(sum)}
+//     `;
+
+//     alert(`Round off of ${Math.sqrt(sum)} is ${Math.round(Math.sqrt(sum))}`);
+//     response = `Round off  of ${Math.sqrt(sum)} is ${Math.round(Math.sqrt(sum))}
+//     `;
+//     output.innerHTML = response;
+//   }
+// };
+// AddNum();
+
+number = (myNumber) => {
+  while (myNumber !== 13) {
+    myNumber = prompt("Enter your number: ");
+    if (myNumber === "") {
+      alert("Please, do not leave empty");
+      response = "Please, do not leave empty";
+    } else if (myNumber <= 10) {
+      alert("Number must not be lower than 13");
+      response = "Number must not be lower than 13";
+    } else if (myNumber >= 15) {
+      alert("Number must not be higher than 15");
+      response = "Number must not be higher than 15";
+    } else {
+      alert(`You enter ${myNumber} favorite`);
+      response = `You enter ${myNumber} favorite`;
+    }
+    break;
+  }
+  console.log(response);
+};
+number();
+
+// function getCarDetails() {
+//   return `The ${Cars.Toyota.model} was made in ${Cars.Toyota.made_in} in the year ${Cars.Toyota.year}`;
+// }
+// console.log(getCarDetails());
