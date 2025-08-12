34a6096454bab81a84b6c30a848a97b12bd5eb8b
Jayson-PAmano <jaysonpa36@gmail.com>
2025-08-11 23:33:28 -0700

    JS Practice

---
+++ Js-practice/js/Practice10.js
@@ -0,0 +1,12 @@
+let fruits = ["apple", "orange", "grapes"];
+let colors = ["red", "green", "blue", "yellow"];
+let text = "Heroes";
+
+let counter = 0;
+let counter2 = 5;
+
+let numbers = [1, 2, 3, 4];
+let numbers2 = [21, 32, 13, 54];
+
+const letters = ["a", "b", "c", "d"];
+const letters2 = ["e", "f", "g", "h"];
