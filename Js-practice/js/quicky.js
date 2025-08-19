c17f69c37676a3f13d4658a9b23abac5adcd9bb5
Jayson-PAmano <jaysonpa36@gmail.com>
2025-08-18 23:52:03 -0700

    JS quicky

--- Js-practice/js/quicky.js
+++ Js-practice/js/quicky.js
@@ -69,19 +69,19 @@ const Person = [
 // for (lete of letters) {
 //   logLetters(letters.indexOf(lete), lete);
 // }
-console.log("\n");
+// console.log("\n");
 
-function logLetters(element, index) {
-  console.log(`No. ${index}\n Letter -> ${element.toUpperCase()}`);
-}
-for (i = 0; i < letters.length; i++) {
-  logLetters(letters[i], i);
-}
-console.log("\n");
-letters2.forEach((element, index) => {
-  console.log(`No. ${index}\n Letter -> ${element.toUpperCase()}`);
-});
-console.log("\n");
+// function logLetters(element, index) {
+//   console.log(`No. ${index}\n Letter -> ${element.toUpperCase()}`);
+// }
+// for (i = 0; i < letters.length; i++) {
+//   logLetters(letters[i], i);
+// }
+// console.log("\n");
+// letters2.forEach((element, index) => {
+//   console.log(`No. ${index}\n Letter -> ${element.toUpperCase()}`);
+// });
+// console.log("\n");
 
 /*Using forEach with Array Object*/
 
@@ -91,6 +91,58 @@ console.log("\n");
 //   );
 // });
 
-for (i = 0; i < Person.length; i++) {
-  console.log(`Id#. ${i}\n Name -> ${Person[i].name} \n`);
+// for (i = 0; i < Person.length; i++) {
+//   console.log(`Id#. ${i}\n Name -> ${Person[i].name} \n`);
+// }
+console.log("\n", "\n");
+getName = (name1, name2) => {
+  name1 = "Jaden";
+  name2 = "Aster";
+
+  console.log(`Hello, ${name1} & ${name2}`);
+};
+callName = () => {
+  getName();
+  console.log("Nice to meet you!");
+};
+callName();
+
+getSum = (n1, n2) => n1 + n2;
+console.log(getSum(5, 5));
+
+doubleSum = (a, b) => {
+  let newSum = getSum(a, b) * 2;
+  console.log(newSum);
+};
+doubleSum(5, 5);
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
 }
+console.log("\n");
+while (counter2 >= 0) {
+  console.log(counter2);
+  counter2--;
+}
+
+console.log("\n");
+
+for (let i = 0; i < colors.length; i++) {
+  console.log(`Index: ${i} \n Color: ${colors[i]}`);
+}
+console.log("\n");
+
+for (let i = colors.length - 1; i >= 0; i--) {
+  console.log(`Index: ${i} \n Color: ${colors[i]}`);
+}
+
+for (i = 0; i < letters.length; i++) {
+  logLetters(letters[i], i);
+}
+function logLetters(element, index) {
+  console.log(index);
+  console.log(element);
+}
+
+// Tobe Continued......
