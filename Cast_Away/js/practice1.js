e2028419032cfd559687570faf0aa6a5c4f2f9dd
Jayson-PAmano <jaysonpa36@gmail.com>
2025-09-17 22:45:14 -0700

    getting used to it again

--- Cast_Away/js/practice1.js
+++ Cast_Away/js/practice1.js
@@ -30,14 +30,60 @@ function myFunction(p1, p2) {
 }
 console.log(myFunction(5, 5));
 
-myFunction2 = (p1, p2) => {
-  return p1 + p2;
-};
-console.log(myFunction2(2, 2) * 2);
+// myFunction2 = (p1, p2) => {
+//   return p1 + p2;
+// };
+// console.log(myFunction2(2, 2) * 2);
 
 getSum = (num1, num2) => num1 + num2;
 console.log(getSum(23, 4));
-doubleSum = (results) => {
-  results = getSum();
+doubleSum = (a, b) => {
+  return getSum(a, b) * 2;
 };
-console.log(doubleSum(23, 4) * 2);
+console.log(doubleSum(23, 4));
+getName = (name1, name2) => {
+  name1 = "Jayden";
+  name2 = "Aster";
+
+  console.log(`Hello, ${name1} and ${name2}`);
+};
+
+callName = () => {
+  return getName(), "Nice to meet you!";
+};
+console.log(callName());
+
+let results;
+
+getRemainder = (R1) => {
+  if (R1 % 2 === 0) {
+    results = "Odd";
+    return results;
+  } else {
+    results = "Even";
+    return results;
+  }
+};
+console.log(getRemainder(3));
+
+//Loops
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+console.log("\n");
+
+while (counter2 >= 0) {
+  console.log(counter2);
+  counter2--;
+}
+console.log("\n");
+
+for (let i = 0; i < colors.length; i++) {
+  console.log(`Index: ${i} \n Color: ${colors[i]}`);
+}
+console.log("\n");
+for (let i = colors.length - 1; i >= 0; i--) {
+  console.log(`Index: ${i} \n Color: ${colors[i]}`);
+}
